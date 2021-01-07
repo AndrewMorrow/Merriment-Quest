4da79cd094845c3de7261c8840934577ec2a6d0b
AndrewMorrow <beardedmongo@gmail.com>
2021-01-07 16:50:40 -0600

    Updates homepage styling

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,4 +2,7 @@ module.exports = {
     format_date: (date) => {
         return date.toLocaleDateString();
     },
+    check_element: (elemId) => {
+        return document.getElementById(`${elemId}`).innerHTML === "";
+    },
 };
