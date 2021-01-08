a08cfd683b745e5c4592e8e3df953ad09e539457
AndrewMorrow <beardedmongo@gmail.com>
2021-01-08 12:42:15 -0600

    updates readme

--- models/index.js
+++ models/index.js
@@ -1,11 +1,23 @@
 const User = require("./User");
 const Watchlist = require("./Watchlist");
-// const { DataTypes } = require("sequelize");
+const { DataTypes } = require("sequelize");
 
 // associations will go here
-Watchlist.belongsTo(User, {});
+Watchlist.belongsTo(User, {
+    foreignKey: {
+        name: "watchlist_user_id",
+        allowNull: false,
+        type: DataTypes.UUID,
+    },
+});
 
-User.hasMany(Watchlist, {});
+User.hasMany(Watchlist, {
+    foreignKey: {
+        name: "watchlist_user_id",
+        onDelete: "CASCADE",
+        type: DataTypes.UUID,
+    },
+});
 
 // export all models here
 module.exports = { User, Watchlist };
