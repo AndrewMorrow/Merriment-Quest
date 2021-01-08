206ed558e27eb28a6e978084ebe1db8e4c8326da
AndrewMorrow <beardedmongo@gmail.com>
2021-01-08 00:19:19 -0600

    Updates model foreign keys to please heroku

--- models/index.js
+++ models/index.js
@@ -1,19 +1,17 @@
 const User = require("./User");
 const Watchlist = require("./Watchlist");
-const { DataTypes } = require("sequelize");
+// const { DataTypes } = require("sequelize");
 
 // associations will go here
 Watchlist.belongsTo(User, {
     foreignKey: {
         allowNull: false,
-        type: DataTypes.UUID,
     },
 });
 
 User.hasMany(Watchlist, {
     foreignKey: {
         onDelete: "CASCADE",
-        type: DataTypes.UUID,
     },
 });
 
