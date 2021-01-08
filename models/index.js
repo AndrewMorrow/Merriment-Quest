227fbbdf576df04451e02e4c5c34c3dfbb56d224
AndrewMorrow <beardedmongo@gmail.com>
2021-01-08 00:32:59 -0600

    another attempt to please heroku

--- models/index.js
+++ models/index.js
@@ -3,13 +3,9 @@ const Watchlist = require("./Watchlist");
 // const { DataTypes } = require("sequelize");
 
 // associations will go here
-Watchlist.belongsTo(User, {
-    allowNull: false,
-});
+Watchlist.belongsTo(User, {});
 
-User.hasMany(Watchlist, {
-    onDelete: "CASCADE",
-});
+User.hasMany(Watchlist, {});
 
 // export all models here
 module.exports = { User, Watchlist };
