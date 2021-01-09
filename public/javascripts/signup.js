c79116105cdf80812d726a6b3c428c878576a32e
AndrewMorrow <beardedmongo@gmail.com>
2021-01-08 21:44:06 -0600

    Password bug hotfix

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -8,31 +8,32 @@ const createCredentials = async (e) => {
     e.preventDefault();
     if ($userPassword.val().trim() !== $userPasswordCheck.val().trim()) {
         alert("Passwords do not match.");
-    }
-    if (
-        $userName.val().trim().length >= 4 &&
-        $userPassword.val().trim().length
-    ) {
-        const credentials = {
-            user_name: $userName.val().trim(),
-            user_password: $userPassword.val().trim(),
-        };
-
-        if (credentials.user_name && credentials.user_password) {
-            const response = await fetch("/api/user/", {
-                method: "POST",
-                body: JSON.stringify(credentials),
-                headers: { "Content-Type": "application/json" },
-            });
-            if (response.ok) {
-                // If successful, redirect the browser to the dashboard page
-                document.location.replace("/dashboard");
-            } else {
-                alert("Something went wrong, please try again");
-            }
-        }
     } else {
-        alert("Please check Requirements for each field.");
+        if (
+            $userName.val().trim().length >= 4 &&
+            $userPassword.val().trim().length
+        ) {
+            const credentials = {
+                user_name: $userName.val().trim(),
+                user_password: $userPassword.val().trim(),
+            };
+
+            if (credentials.user_name && credentials.user_password) {
+                const response = await fetch("/api/user/", {
+                    method: "POST",
+                    body: JSON.stringify(credentials),
+                    headers: { "Content-Type": "application/json" },
+                });
+                if (response.ok) {
+                    // If successful, redirect the browser to the dashboard page
+                    document.location.replace("/dashboard");
+                } else {
+                    alert("Something went wrong, please try again");
+                }
+            }
+        } else {
+            alert("Please check Requirements for each field.");
+        }
     }
 };
 
